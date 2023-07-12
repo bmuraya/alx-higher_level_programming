82942c2ce837d1841191315e82b455822fbd572e
bmuraya <babumuraya@gmail.com>
2023-07-12 18:06:06 +0300

    write file task 1

--- 0x0B-python-input_output/1-write_file.py
+++ 0x0B-python-input_output/1-write_file.py
@@ -9,3 +9,4 @@ def write_file(filename="", text=""):
     """writes to text file and returns num chars written"""
     with open(filename, mode="w", encoding="utf-8") as f:
         return (f.write(text))
+    
