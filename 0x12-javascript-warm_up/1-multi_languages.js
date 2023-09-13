7ee601c340422cf249aaf051d4f1591adfe14193
bmuraya <babumuraya@gmail.com>
2023-09-13 21:00:51 +0300

    task file

--- 0x12-javascript-warm_up/1-multi_languages.js
+++ 0x12-javascript-warm_up/1-multi_languages.js
@@ -1,9 +1,4 @@
-#!/usr/bin/node
-<<<<<<< HEAD
-// script that prints 3 lines
-console.log('C is fun\nPython is cool\nJavascript is amazing');
-=======
-/* three print ling on js*/
+#!/usr/bin/env node
 
 console.log('C is fun');
 console.log('Python is cool');
--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,4 +1,5 @@
-#!/usr/bin/node
+#!/usr/bin/env node
+
 /* script that prints a message depending
 of the number of arguments passed */
 const numArgv = process.argv.length;
