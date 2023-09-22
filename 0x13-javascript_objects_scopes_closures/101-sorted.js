b39cd8afb95a530b8284ae0e1846645055f95fdf
bmuraya <babumuraya@gmail.com>
2023-09-22 14:56:22 +0300

    semistandard file

--- 0x13-javascript_objects_scopes_closures/101-sorted.js
+++ 0x13-javascript_objects_scopes_closures/101-sorted.js
@@ -1,9 +1,10 @@
 #!/usr/bin/node
+
 /* script that imports a dictionary of occurrences by user id
 and computes a dictionary of user ids by occurrence */
 const dict = require('./101-data').dict;
 const newDict = {};
-Object.keys(dict).map(function (key, index) {
+Object.keys(dict).forEach(function (key) {
   if (newDict[dict[key]] === undefined) {
     newDict[dict[key]] = [];
   }
